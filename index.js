e753b4607d366df6627385a9d764e35048cb4485
aish51197 <aish51197@gmail.com>
2018-11-02 14:14:23 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+ // $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// })
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:50,
+    center:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:true
+        },
+        1400:{
+            items:4,
+            nav:true,
+            loop:true
+        },
+        1500:{
+            items:5,
+            nav:true,
+            loop:true
+        }
+    }
+})
+
+$('owl-carousel.carousel-2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
